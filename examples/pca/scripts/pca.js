69d6cac0d1a84c22d3a9d10827a274f2bff57488
Srinivasan Ramachandran <ashok_srinivasan2002@yahoo.com>
2014-07-26 12:12:08 -0400

    Updates on PCA

--- examples/pca/scripts/pca.js
+++ examples/pca/scripts/pca.js
@@ -1,31 +1,44 @@
-var scene, originalCubeMaterial, pcaCubeMaterial, originalCube, pcaCube, covarianceMatrix;
+var scene, originalCubeMaterial, originalCube, covarianceMatrix;
+
 function render3D()
 {
     requestAnimationFrame(render3D);
     $("#editor").threeworld('render');
 }
 
-function secondOrderCovarianceMatrix()
+function secondOrderCovarianceMatrix(mesh)
 {
     var matrixSum = new THREE.Matrix3(0, 0, 0, 0, 0, 0, 0, 0, 0);
     var covariances = [];
-    var n = pcaCube.geometry.vertices.length;
-
+    var n = mesh.geometry.vertices.length;
+    
+    //Calculation of a covariance matrix from a vector v = [x y z] is by
+    //|v.x * v.x , v.x* v.y , v.x * v.z|
+    //|v.x * v.x , v.x* v.y , v.x * v.z|
+    //|v.x * v.x , v.x* v.y , v.x * v.z|
+    //That results in a 3x3 matrix from a 1x3 vector/point
+    //So if a mesh has 10 vertices calcualte all the 10 covariance matrix from 
+    //the 10 vectors and sum them all up. This will result in a 3x3 matrix.
+    //Then multiply each of the elements in this 3x3 matrix with the scalar value
+    // 1 / totalVerticesInMesh to get the covariance matrix;
+    
     for (var i = 0; i < n; i++)
     {
-        var v = pcaCube.geometry.vertices[i];
+        var v = mesh.geometry.vertices[i];
         var matrix = new THREE.Matrix3();
 
-        matrix.set(v.x * v.x, v.x * v.y, v.x * v.z,
-                v.y * v.x, v.y * v.y, v.y * v.z,
-                v.z * v.x, v.z * v.y, v.z * v.z);
+        matrix.set(
+                    v.x * v.x, v.x * v.y, v.x * v.z,
+                    v.y * v.x, v.y * v.y, v.y * v.z,
+                    v.z * v.x, v.z * v.y, v.z * v.z
+                    );
         covariances.push(matrix);
     }
 
     for (var i = 0; i < covariances.length - 1; i++)
     {
         var covarianceM = covariances[i];
-        for (var j = 0;j < matrixSum.elements.length; j++)
+        for (var j = 0; j < matrixSum.elements.length; j++)
         {
             matrixSum.elements[j] += covarianceM.elements[j];
         }
@@ -35,44 +48,23 @@ function secondOrderCovarianceMatrix()
     {
         matrixSum.elements[i] *= 1 / n;
     }
-    
-    console.log(matrixSum, matrixSum.transpose());
     return matrixSum;
 }
 
-function drawPCA(covarianceMatrix)
-{
-//    var eigenVectors = [];
-    var lineMaterial = new THREE.LineBasicMaterial({color: 0xFFFFFF});
-    
-    var eigenVectors = [
-        [-1 , -1 , 1],
-        [1 , 0 , 1],
-        [-1 , 1 , 0]
-    ];
-    
-    
-    for(var i=0;i<3;i++)
-    {
-//        var eigenVector = new THREE.Vector3(covarianceMatrix.elements[i],covarianceMatrix.elements[i+3],covarianceMatrix.elements[i+6]);
-        var eigenVector = new THREE.Vector3(eigenVectors[i][0],eigenVectors[i][1],eigenVectors[i][2]);
-        var lineGeometry = new THREE.Geometry();
-        var line;
-        lineGeometry.vertices.push(new THREE.Vector3());
-        lineGeometry.vertices.push(eigenVector);
-        line = new THREE.Line(lineGeometry, lineMaterial);
-        
-        scene.add(line);
-        console.log('add line ', eigenVector);
-//        eigenVectors.push(eigenVector);
-    }
-}
-
-function cubeToGeometricalOrigin()
+function calculateMeanMatrix(mesh, updateMesh)
 {
     var origin = new THREE.Vector3();
-    var oneByN = 1 / originalCube.geometry.vertices.length;
-    var newGeometry = originalCube.geometry.clone();
+    var oneByN = 1 / mesh.geometry.vertices.length;
+    var newGeometry = mesh.geometry.clone();
+
+
+    //This step is merely to align a geometry to origin
+    //It uses the mean or average mathematical method
+    //e.g. if array a = [1,2,3,4,17,6,7,8];
+    //The total number of elements = 8;
+    //Then the mean value is (1 + 2 + 3 + 4 + 17 + 6 + 7 + 8) / 8 = 48 / 8 = 6
+    //The Mean Matrix will be ( 1 - 6, 2 - 6, 3 - 6, 4 - 6, 17 - 6, 6 - 6, 7 - 6, 8 - 6)
+    //Resulting in Mean Matrix which is (-5, -4, -3, -2, 11, 0, 1, 2)
 
     for (var i = 0; i < newGeometry.vertices.length; i++)
     {
@@ -85,25 +77,31 @@ function cubeToGeometricalOrigin()
     origin.x = origin.x * oneByN;
     origin.y = origin.y * oneByN;
     origin.z = origin.z * oneByN;
-
+    
     for (var i = 0; i < newGeometry.vertices.length; i++)
     {
         var vertex = newGeometry.vertices[i];
-        vertex.x = vertex.x - origin.x;
-        vertex.y = vertex.y - origin.y;
-        vertex.z = vertex.z - origin.z;
+        vertex.x -= origin.x;
+        vertex.y -= origin.y;
+        vertex.z -= origin.z;
     }
-    pcaCubeMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00, wireframe: true});
-    pcaCube = new THREE.Mesh(newGeometry, pcaCubeMaterial);
-    scene.add(pcaCube);
-    console.log(oneByN, origin);
-}
 
-function applyPCAForCube()
-{
-    cubeToGeometricalOrigin();
-    covarianceMatrix = secondOrderCovarianceMatrix();
-    drawPCA(covarianceMatrix);
+    //The below step is equivalent to iterating through all the vertices of 
+    //the geometry and reducing the origin.x, origin.y and origin.z values 
+    //from the vertices. 
+    //e.g. geometry.vertices[i].x - origin.x
+    //e.g. geometry.vertices[i].y - origin.y
+    //e.g. geometry.vertices[i].z - origin.z
+
+//    newGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(new THREE.Vector3(-origin.x, -origin.y, -origin.z)));
+
+    if (updateMesh)
+    {
+        mesh.geometry = newGeometry.clone();
+        mesh.geometry.verticesNeedUpdate = true;
+//        
+    }
+    return newGeometry;
 }
 
 function showCubeForPCA()
@@ -120,10 +118,11 @@ function main()
     var w = window.innerWidth;
     var h = window.innerHeight;
 //    $("#editor").threeworld({worldwidth: w * 1, worldheight: h * 1, columns: 2, axis: false, views: {types: [FRONT_VIEW, TOP_VIEW, SIDE_VIEW, FREE_VIEW]}}).on('meshloadcomplete', function(e)
-    $("#editor").threeworld({worldwidth: w * 1, worldheight: h * 1, columns: 2, axis: false, floor: true, defaultlights: true, views: {types: [FREE_VIEW, SIDE_VIEW, FRONT_VIEW, TOP_VIEW]}});
+//    $("#editor").threeworld({worldwidth: w * 1, worldheight: h * 1, columns: 2, axis: false, floor: true, defaultlights: true, views: {types: [FREE_VIEW, SIDE_VIEW, FRONT_VIEW, TOP_VIEW]}});
+    $("#editor").threeworld({worldwidth: w * 1, worldheight: h * 1, columns: 2, axis: false, floor: true, defaultlights: true, views: {types: [FREE_VIEW]}});
     scene = $("#editor").threeworld('get', 'scene');
     showCubeForPCA();
-    applyPCAForCube();
+    calculateMeanMatrix(originalCube, true);
     render3D();
 }
 $(document).ready(main);
