8f4933298cdc6c67389832635a11ba86b2a2cbf9
Srinivasan Ramachandran <ashok_srinivasan2002@yahoo.com>
2014-07-30 08:31:08 -0400

    Updates on voxelization example

--- examples/voxelization/scripts/voxelization.js
+++ examples/voxelization/scripts/voxelization.js
@@ -1,5 +1,16 @@
 var scene, model, mesh;
-var voxels;
+var voxels=
+        {
+            voxels: [], 
+            totalwidth: 0, 
+            totalheight: 0, 
+            totaldepth: 0, 
+            unitwidth: 0,
+            unitheight: 0,
+            unitdepth: 0,
+            totalvoxels: 0
+        };
+var resolution = 30;
 
 function render3D()
 {
@@ -7,25 +18,96 @@ function render3D()
     $("#editor").threeworld('render');
 }
 
+function initializeVoxelMap(bounds)
+{
+    var width = bounds.max.x - bounds.min.x, height = bounds.max.y - bounds.min.y, depth = bounds.max.z - bounds.min.z;
+    var unitwidth = (width / resolution), unitheight = (height / resolution), unitdepth = (depth / resolution);
+    
+    voxels=
+        {
+            allvoxels : [],
+            voxels: [], 
+            startx: bounds.min.x,
+            starty: bounds.min.y,
+            startz: bounds.min.z,
+            totalwidth: width, 
+            totalheight: height, 
+            totaldepth: depth, 
+            unitwidth: unitwidth,
+            unitheight: unitheight,
+            unitdepth: unitdepth,
+            totalvoxels: 0
+        };
+}
+
+function getVoxel(xindex, yindex, zindex)
+{
+    var id = 'voxel_'+xindex+'_'+yindex+'_'+zindex;    
+    
+    if(voxels[id] === undefined)
+    {
+        voxels[id] = {id: id, vertices:[],xindex:xindex, yindex: yindex, zindex: zindex,x :0, y:0, z:0};
+        voxels.voxels.push(voxels[id]);
+        voxels.totalvoxels++;
+    }
+    return voxels[id];
+}
+
+function drawVoxel(x, y, z)
+{
+    var mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(voxels.unitwidth, voxels.unitheight, voxels.unitdepth, 1, 1, 1), 
+            new THREE.MeshBasicMaterial({color: 0x00FF00, wireframe: false})
+                    );
+    mesh.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(x, y, z));
+    
+    scene.add(mesh);
+}
+
+function createVoxel(vertex, vertexindex)
+{
+    var sx = voxels.startx, sy = voxels.starty, sz = voxels.startz;
+    var unitwidth = voxels.unitwidth, unitheight = voxels.unitheight, unitdepth = voxels.unitdepth;
+    var xindex = Math.floor((vertex.x - sx) / unitwidth);
+    var yindex = Math.floor((vertex.y - sy) / unitheight);
+    var zindex = Math.floor((vertex.z - sz) / unitdepth);
+    
+    var voxel = getVoxel(xindex, yindex, zindex);
+    var vertexinfo = new Object();
+    
+    voxel.x = sx + (xindex * unitwidth);
+    voxel.y = sy + (yindex * unitheight);
+    voxel.z = sz + (zindex * unitdepth);
+    
+    vertexinfo.index = vertexindex;
+    vertexinfo.vertex = vertex;
+    vertexinfo.xindex = xindex;
+    vertexinfo.yindex = yindex;
+    vertexinfo.zindex = zindex;
+    
+    voxel.vertices.push(vertexinfo);
+}
+
 function createVoxelMap(mesh)
 {
     var geometry = mesh.geometry;
-    var iterationLogic = (geometry.vertices.length < geometry.faces.length) ? 'v' : 'f';
-    var iterationLength = Math.min(geometry.vertices.length, geometry.faces.length);
-    var bounds = geometry.boundingBox;
-    var face, vertex;
+    var iterationLength = geometry.vertices.length;
+    var bounds = geometry.boundingBox;    
+    initializeVoxelMap(bounds);
     
     for(var i=0;i<iterationLength;i++)
     {
-        if(iterationLogic === 'f')
-        {
-            
-        }
+        var vertex = geometry.vertices[i];
+        createVoxel(vertex, i);
     }
     
-    
-    console.log(geometry.vertices.length);
-    console.log(geometry.faces.length);
+    for(var i=0;i<voxels.voxels.length;i++)
+    {
+        var voxel = voxels.voxels[i];
+        drawVoxel(voxel.x, voxel.y, voxel.z);
+    }
+//    mesh.visible = false;
+    console.log('TOTAL VOXELS ::: ', voxels.voxels.length);
 }
 
 function createBoxHelper(color, bounds, inboundMesh)
@@ -43,6 +125,35 @@ function createBoxHelper(color, bounds, inboundMesh)
     return cube;
 }
 
+
+function calculateMeanMatrix(mesh, updateMesh)
+{
+    var origin = new THREE.Vector3();
+    var oneByN = 1 / mesh.geometry.vertices.length;
+    var newGeometry = mesh.geometry.clone();
+
+    for (var i = 0; i < newGeometry.vertices.length; i++)
+    {
+        var vertex = newGeometry.vertices[i];
+        origin.x += vertex.x;
+        origin.y += vertex.y;
+        origin.z += vertex.z;
+    }
+
+    origin.x = origin.x * oneByN;
+    origin.y = origin.y * oneByN;
+    origin.z = origin.z * oneByN;
+    
+    newGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(-origin.x, -origin.y, -origin.z));
+    
+    if (updateMesh)
+    {
+        mesh.geometry = newGeometry.clone();
+        mesh.geometry.verticesNeedUpdate = true;
+    }
+    return newGeometry;
+}
+
 function onModelLoaded(e)
 {
     model = e.model;
@@ -52,8 +163,12 @@ function onModelLoaded(e)
         {
 //            child.geometry.applyMatrix(new THREE.Matrix4().makeScale(0.1,0.1,0.1));
             mesh = child;
+            calculateMeanMatrix(child, true);
+            
+            child.geometry.computeBoundingBox();
             child.updateMatrix();
             child.updateMatrixWorld();
+            
             scene.add(createBoxHelper(0x0000FF, child.geometry.boundingBox, child));
             return;
         }
@@ -63,7 +178,7 @@ function onModelLoaded(e)
 
 function loadModel()
 {
-    $("#editor").threeworld('load', 'http://localhost/models/CaptainAmericaNormal.obj', 'obj');
+    $("#editor").threeworld('load', 'http://localhost/models/al.obj', 'obj');
 }
 
 function main()
